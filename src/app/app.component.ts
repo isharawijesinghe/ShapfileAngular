c1f76bb973d504ebe072a55f7768243a1aa17250
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2018-10-13 15:11:42 +0530

    Create seperate components for Map rendering and Map details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import * as JSZip from 'jszip';
 
 declare var require: any
 
-var JSZip = require("jszip");
+
 
 
 @Component({
@@ -18,19 +18,15 @@ var JSZip = require("jszip");
 
 export class AppComponent implements OnInit {
 
-    
-
-	constructor() {
-
-	}
+	constructor() {}
 
 	ngOnInit() {
 		const map = L.map('map').setView([6.80,80.400] , 10);
 		L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {
 			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
-        
-       
+
+
         // var zip = new JSZip();
 
         // var new_zip = new JSZip();
@@ -41,27 +37,37 @@ export class AppComponent implements OnInit {
 //     console.log(zip)
 //     new_zip.file("hello.txt").async("string"); // a promise of "Hello World\n"
 // });
-	
+
         // 	var m = L.map('map').setView([42.09618442380296, -71.5045166015625], 8);
 		// var watercolor = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 		// }).addTo(m);
-		
-		var base = '../../assets/shp.zip';
 
-	
-		
+		var base = '../../assets/landslide.zip';
+
+
+
 		var shpfile = new L.Shapefile(base, {
 			onEachFeature: function(feature, layer) {
-				
+
 				if (feature.properties) {
 					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+						console.log(k + ": " +feature.properties[k])
 						return k + ": " + feature.properties[k];
 					}).join("<br />"), {
 						maxHeight: 200
 					});
 				}
+
+				layer.on('click', function (e) {
+					// e = event
+					console.log(e.target.feature.properties);
+					// You can make your ajax call declaration here
+					//$.ajax(...
+				  });
 			}
+
+
 		});
 		shpfile.addTo(map);
 		shpfile.once("data:loaded", function() {

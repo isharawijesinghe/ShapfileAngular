7868b78111c21d28f15de4bf6225541e8a9666d5
Yashodha <yashodha.14@cse.mrt.ac.lk>
2018-10-13 21:43:11 +0530

    adding weather deatail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
+import { Daterangepicker } from 'ng2-daterangepicker';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    WeatherDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Daterangepicker
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather-detail/weather-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-detail',
+  templateUrl: './weather-detail.component.html',
+  styleUrls: ['./weather-detail.component.css']
+})
+export class WeatherDetailComponent implements OnInit {
+  public daterange: any = {};
+
+  // see original project for full list of options
+  // can also be setup using the config service to apply to multiple pickers
+  public options: any = {
+    locale: { format: 'YYYY-MM-DD' },
+    alwaysShowCalendars: false,
+  };
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  public selectedDate(value: any, datepicker?: any) {
+    // this is the date the iser selected
+    console.log(value);
+
+    // any object can be passed to the selected event and it will be passed back here
+    datepicker.start = value.start;
+    datepicker.end = value.end;
+
+    // or manupulat your own internal property
+    this.daterange.start = value.start;
+    this.daterange.end = value.end;
+    this.daterange.label = value.label;
+  }
+
+}

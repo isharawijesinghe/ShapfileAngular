2a4252fdff917cee89baac4774673f59f49c43e1
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2018-10-14 10:45:33 +0530

    Map on click notify done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
-
 import { MapComponent } from './map/map.component';
+import { MapdetailComponent } from './map/mapdetail/mapdetail.component';
 
 
 
@@ -13,6 +12,7 @@ import { MapComponent } from './map/map.component';
     AppComponent,
     MapComponent,
     WeatherDetailComponent,
+    MapdetailComponent,
   ],
   imports: [
     BrowserModule,
